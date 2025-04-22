831611d2ef2be1055390bb6f96d23c096f71aa1d
sportsprofessionals <infosportsprofessionals@gmail.com>
2025-04-21 19:18:27 -0500

    next

--- next.config.mjs
+++ next.config.mjs
@@ -27,60 +27,6 @@ const nextConfig = {
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
   },
-  async redirects() {
-    return [
-      // Redirecciones a páginas similares
-      {
-        source: "/noticias",
-        destination: "/", // No hay equivalente en el sitemap, redirige al home
-        permanent: true, // Redirección 301
-      },
-      {
-        source: "/servicios-deportivos",
-        destination: "/servicios/servicios-deportivos",
-        permanent: true,
-      },
-      {
-        source: "/servicios-recreativos",
-        destination: "/servicios/servicios-recreativos",
-        permanent: true,
-      },
-      {
-        source: "/administracion-deportiva",
-        destination: "/servicios/administracion-deportiva",
-        permanent: true,
-      },
-      {
-        source: "/servicios-en-actividad-fisica",
-        destination: "/servicios/actividad-fisica",
-        permanent: true,
-      },
-      {
-        source: "/home-new-3-4-3",
-        destination: "/", // Redirige versiones de prueba al home
-        permanent: true,
-      },
-      {
-        source: "/2024/06/13/consejos-antes-de-hacer-actividad-fisica",
-        destination: "/", // No hay sección de blog en el sitemap, redirige al home
-        permanent: true,
-      },
-      // Redirección fallback para capturar URLs antiguas no mapeadas específicamente
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "header",
-            key: "referer",
-            value:
-              "(.*?sportsprofessionals\\.com\\.co/(?!servicios|quienes-somos|contacto|formacion|escenarios-deportivos|clientes|politica-de-cookies).*)",
-          },
-        ],
-        destination: "/", // Redirige al home cualquier URL antigua que no exista en el nuevo sitio
-        permanent: true,
-      },
-    ];
-  },
 };
 
 if (userConfig) {
